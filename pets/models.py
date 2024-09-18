45af1051d80f1b85829cf865c7d1ac99e444ae09
29Jeanca <jeancarlosbarberena29@gmail.com>
2024-09-18 00:34:27 -0600

    Primera parte

--- pets/models.py
+++ pets/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Owner(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=20)
     email = models.EmailField(unique=True)
     age = models.IntegerField()
@@ -10,3 +11,28 @@ class Owner(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Animal(models.Model):
+    id = models.AutoField(primary_key=True)
+    age = models.IntegerField()
+    color = models.CharField(max_length=100)
+    weight = models.FloatField()
+    species = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.age
+    
+class Pet(models.Model):
+    name = models.CharField(max_length=255)
+    owner = models.ForeignKey(Owner,on_delete=models.CASCADE)
+    animal = models.ForeignKey(Animal,on_delete=models.CASCADE)
+    adoption_date = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.name 
+
+    
